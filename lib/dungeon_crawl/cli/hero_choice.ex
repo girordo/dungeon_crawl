6f89dc366d862a840648a2ab48e331123246397e
girordo <tsgiroldo@gmail.com>
2023-01-12 01:02:22 -0300

    feat(hero_choice.ex): creating hero_choice

--- lib/dungeon_crawl/cli/hero_choice.ex
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -5,10 +5,16 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     Shell.cmd("clear")
     Shell.info("Star by choosing your hero:")
     heroes = DungeonCrawl.Heroes.all()
+    find_hero_by_index = &Enum.at(heroes, &1)
 
     heroes
     |> Enum.map(& &1.name)
-    |> display_options
+    |> display_options()
+    |> generate_question()
+    |> Shell.prompt()
+    |> parse_answer()
+    |> find_hero_by_index.()
+    |> confirm_hero()
   end
 
   def display_options(options) do
@@ -19,5 +25,21 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     end)
 
     options
+  end
+
+  defp generate_question(options) do
+    options = Enum.join(1..Enum.count(options), ",")
+    "Which one? [#{options}\n"
+  end
+
+  defp parse_answer(answer) do
+    {option, _} = Integer.parse(answer)
+    option - 1
+  end
+
+  defp confirm_hero(chosen_hero) do
+    Shell.cmd("clear")
+    Shell.info(chosen_hero.description)
+    if Shell.yes?("Confirm?"), do: chosen_hero, else: start()
   end
 end
